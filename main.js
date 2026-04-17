396c20746e5accb927289c4b5351d5e18931317d
livviasantana <livviasantana@gmail.com>
2026-04-17 18:37:56 +0000

    JS e últimos ajustes do CSS

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// Add JS here
+document.addEventListener('DOMContentLoaded', () => {
+    const agendamentoForm = document.getElementById('agendamento-form');
+
+    agendamentoForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const nome = document.getElementById('nome').value;
+        const telefone = document.getElementById('telefone').value;
+        const email = document.getElementById('email').value;
+
+        console.log('Nome:', nome);
+        console.log('Telefone:', telefone);
+        console.log('Email:', email);
+    });
+});
